dac3323c65489d002ea9b3de39e23f366cf81006
andreyjs26 <andreyjs260393@gmail.com>
2017-07-04 15:25:33 -0600

    first commit

---
+++ Employee_Shop/BusinessLogic/EmployeeManagement.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class EmployeeManagement
+    {
+
+       private EmployeeShopEntities employee = new EmployeeShopEntities();
+
+       public void InsertEmployee(Object obj)
+       {
+           if (obj != null) {
+
+               employee.tbl_Employee.Add(obj);
+           
+           }       
+       }
+
+       public void DeleteEmployee(int id)
+       {
+           var obj = employee.tbl_Employee.Find(id);
+
+           employee.tbl_Employee.Remove(obj);
+       
+       }
+
+       public Object RetrieveSpecificEmployee(int id)
+       {
+           var obj = employee.tbl_Employee.Find(id);
+
+           return obj;
+       }
+
+       public List<Object> RetrieveListEmployee()
+       {
+           var result = employee.tbl_Employee.FindAll();
+           return result;
+       }
+
+
+    }
+}
